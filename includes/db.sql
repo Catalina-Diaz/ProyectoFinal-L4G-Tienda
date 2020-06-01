8a1acd22c16e71e3d0ebd524868c95af12571cd0
diana yela <Dianayela672@gmail.com>
2020-05-31 23:24:51 -0500

    tallerfinal

--- includes/db.sql
+++ includes/db.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         localhost
--- Versión del servidor:         5.7.19 - MySQL Community Server (GPL)
+-- Versión del servidor:         5.7.24 - MySQL Community Server (GPL)
 -- SO del servidor:              Win64
--- HeidiSQL Versión:             9.4.0.5125
+-- HeidiSQL Versión:             10.2.0.5599
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -11,33 +11,10 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Volcando estructura para tabla tienda.crearuser
-CREATE TABLE IF NOT EXISTS `crearuser` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombres` varchar(50) DEFAULT NULL,
-  `correo` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `celular` varchar(50) DEFAULT NULL,
-  `direccion` varchar(50) DEFAULT NULL,
-  `ciudad` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
-
 -- Volcando datos para la tabla tienda.crearuser: ~0 rows (aproximadamente)
 DELETE FROM `crearuser`;
 /*!40000 ALTER TABLE `crearuser` DISABLE KEYS */;
 /*!40000 ALTER TABLE `crearuser` ENABLE KEYS */;
-
--- Volcando estructura para tabla tienda.productos
-CREATE TABLE IF NOT EXISTS `productos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` text,
-  `precio` text,
-  `descripcion` text,
-  `categoria` text,
-  `imagen` longblob,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla tienda.productos: ~2 rows (aproximadamente)
 DELETE FROM `productos`;
@@ -47,38 +24,22 @@ INSERT INTO `productos` (`id`, `nombre`, `precio`, `descripcion`, `categoria`, `
 	(10, 'bslsod', '9767', 'jdjdo', 'bdixzk', NULL);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 
--- Volcando estructura para tabla tienda.sesion
-CREATE TABLE IF NOT EXISTS `sesion` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `correo` varchar(50) NOT NULL,
-  `passwor` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf32;
-
--- Volcando datos para la tabla tienda.sesion: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla tienda.sesion: ~1 rows (aproximadamente)
 DELETE FROM `sesion`;
 /*!40000 ALTER TABLE `sesion` DISABLE KEYS */;
+INSERT INTO `sesion` (`id`, `correo`, `passwor`) VALUES
+	(1, 'diana.en@hotmail.com', '123');
 /*!40000 ALTER TABLE `sesion` ENABLE KEYS */;
 
--- Volcando estructura para tabla tienda.usuarios
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` text NOT NULL,
-  `password` text NOT NULL,
-  `telefono` text NOT NULL,
-  `ciudad` text NOT NULL,
-  `direccion` text NOT NULL,
-  `estado` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
--- Volcando datos para la tabla tienda.usuarios: ~2 rows (aproximadamente)
+-- Volcando datos para la tabla tienda.usuarios: ~5 rows (aproximadamente)
 DELETE FROM `usuarios`;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` (`id`, `username`, `password`, `telefono`, `ciudad`, `direccion`, `estado`) VALUES
-	(1, 'carlos', '1234', '3122302446', 'Valle de Sibundoy', 'San Francisco', 'admin'),
-	(2, 'Duvan', '1234', '22', 'Mocoa', 'Ssndkanals', 'ventas'),
-	(3, 'catalina', 'diaz', '328', 'mocoa', 'b am', 'ventas');
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `password`, `telefono`, `ciudad`, `direccion`) VALUES
+	(1, '0', 'carlos', '1234', '3122302446', 'Valle de Sibundoy', 'San Francisco'),
+	(2, '0', 'Duvan', '1234', '22', 'Mocoa', 'Ssndkanals'),
+	(3, '0', 'catalina', 'diaz', '328', 'mocoa', 'b am'),
+	(4, 'angie', 'diana.en@outlook.com', '12', '12', '12', '23'),
+	(5, 'diana', 'J.JHONATAN@GMAILCOM', 'yela', '1234566', 'mocoa', '878');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
